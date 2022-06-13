e7c0bc5e987745be5877eeb0be039110e61621e5
Alphy Gacheru <alphygacheru@gmail.com>
2022-06-13 20:11:02 +0300

    Add classes to overlay links and implement js logic to hide overlay on overlay link click

--- main.js
+++ main.js
@@ -2,11 +2,28 @@ const menu = document.querySelector('.menu-icon');
 const overlay = document.querySelector('.overlay');
 
 const closeButton = document.querySelector('.close-button');
+const overlayLinkOne = document.querySelector('.overlay-link-one')
+const overlayLinkTwo = document.querySelector('.overlay-link-two')
+const overlayLinkThree = document.querySelector('.overlay-link-three')
+
+
 
 menu.addEventListener('click', ()=>{
     overlay.style.display ="block"
 })
 
 closeButton.addEventListener('click', ()=>{
+    overlay.style.display ="none"
+})
+
+overlayLinkOne.addEventListener('click', ()=>{
+    overlay.style.display ="none"
+})
+
+overlayLinkTwo.addEventListener('click', ()=>{
+    overlay.style.display ="none"
+})
+
+overlayLinkThree.addEventListener('click', ()=>{
     overlay.style.display ="none"
 })
