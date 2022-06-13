62d7e44d934e82b10a320e2fbc77f6ff6b8d9981
Oyebade Priscilla <oyebadepriscilla22@gmail.com>
2022-06-13 16:47:10 +0100

    Add js logic for showing the overlay

---
+++ main.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector('.menu-icon');
+const overlay = document.querySelector('.overlay');
+
+menu.addEventListener('click', ()=>{
+    overlay.style.display ="block"
+})
