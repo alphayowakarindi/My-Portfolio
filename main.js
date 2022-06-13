970eb0e79c51ba3b7acf3ab76254cbe3e207741c
Alphy Gacheru <alphygacheru@gmail.com>
2022-06-13 18:55:07 +0300

    Add js logic for hidding the overlay on click event

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const menu = document.querySelector('.menu-icon');
 const overlay = document.querySelector('.overlay');
 
+const closeButton = document.querySelector('.close-button');
+
 menu.addEventListener('click', ()=>{
     overlay.style.display ="block"
+})
+
+closeButton.addEventListener('click', ()=>{
+    overlay.style.display ="none"
 })
