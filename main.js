cd0bd9e87d254c9dbabe332270ba77141b08d346
Oyebade Priscilla <oyebadepriscilla22@gmail.com>
2022-06-13 16:36:35 +0100

    Revert "Add js logic"
    This reverts commit 596c706ca16bf01cd95fa209d9a3dd8a8785a980.

--- main.js
+++
@@ -1,6 +0,0 @@
-const menu = document.querySelector('.menu-icon');
-const overlay = document.querySelector('.overlay');
-
-menu.addEventListener('click', ()=>{
-    overlay.style.display ="block"
-})
